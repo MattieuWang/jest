f4c4f0e3bff56649d5521b5f203f14ba2bf223f2
MattieuWang <45388728+MattieuWang@users.noreply.github.com>
2019-11-11 19:54:29 +0100

    Add files via upload

--- jest/Jest.java
+++ jest/Jest.java
@@ -1,58 +1,63 @@
 package com.jest;
 
 import com.jest.Controller.Controller;
+import com.jest.carte.Carte;
+import com.jest.joueur.Joueur;
 
 import java.util.Scanner;
 
 public class Jest {
     public static void main(String[] args) throws Throwable {
         int nbJoueur = 1;
+        int round = 1;
         Scanner scanner = new Scanner(System.in);
         Controller controller = new Controller();
         String ordre = "";
+
+
         controller.initJoueur("aaa","a");
         controller.initJoueur("bbb","b");
+        controller.initJoueur("ccc","c");
+        Carte c1 = controller.giveRandomCard(0);
+        Carte c2 = controller.giveRandomCard(0);
 
-        System.out.println("1. Creer un joue");
-        System.out.println("2. Ajouter un joue");
-        System.out.print("votre choix: ");
-        int choix = scanner.nextInt();
-        if(choix == 1)
-        {
-            //creer un serveur
-        }
-        else if(choix == 2)
-        {
-            //rechercher un serveur
-        }
 
-        while (nbJoueur<3)
+
+        while(controller.getCartes().size()-1>0)     // il y a cartes restants dans le paquet
         {
-            System.out.println("1. Creer un joueur virtuel");
-            System.out.println("2. Attendre un joueur");
-            System.out.print("votre choix: ");
-            int choixJoueur = scanner.nextInt();
-            if(choixJoueur == 1)
+            System.out.println("------TOUR "+ round+"-------");
+
+            if(round == 1)  // le 1er tour    deal cards
             {
-                System.out.println("get name");
-                String name = scanner.nextLine();
-                System.out.println("get webIp");
-                String web = scanner.nextLine();
-                controller.initJoueur(name,web);
+                for(Joueur joueur : controller.getJoueurs())
+                {
+                    joueur.ajouterCarte(controller.giveRandomCard(joueur.getId()));
+                    joueur.ajouterCarte(controller.giveRandomCard(joueur.getId()));
+                }
             }
-            else if (choixJoueur == 2)
+            else
             {
-
+                controller.dealCartes();
             }
-        }
+            round ++;
+
+            System.out.println("paquet de cartes: " + controller.getCartes().size());
+
+            controller.listerCarteDeJoueur();
 
 
-        while (!ordre.equals("n"))
-        {
-            System.out.println("Le joue commence !");
-//            System.out.println("Entez l'ordre, s'il vous plait");
-//            ordre = scanner.nextLine();
-            System.out.println("Choisir un carte pour donner aux autres joueurs");
+            //make offer
+            controller.makeOffers();
+
+            //take cards
+            controller.takeCartes();
+
+            controller.listerJestDeJoueur();
+            System.out.println("paquet de cartes: " + controller.getCartes().size());
         }
+
+        // fin de joue
+        controller.finDeJoue();
+
     }
 }
